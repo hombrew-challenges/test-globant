a20d7f8ab211b9838945e7acd195f98f09b59045
Manuel Maldonado <mmaldonadortiz92@gmail.com>
2018-10-13 02:12:04 -0300

    new logs

--- exercise_1/index.js
+++ exercise_1/index.js
@@ -4,10 +4,7 @@ const case0 = sum()
 const case1 = case0(1)
 const case2 = case1()
 const result1 = case2(3)
-const case3 = case0(1, 2)
+const result2 = case0(1, 2)
 
-console.log('sum(): ', case0, '\n\n------\n') // func
-console.log('case0(1): ', case1, '\n\n------\n') // func
-console.log('case1(): ', case2, '\n\n------\n') // func
-console.log('case2(3): ', result1, '\n\n------\n') // 4
-console.log('case0(1, 2): ', case3, '\n\n------\n') // 3
+console.log('curried: ', result1)
+console.log('not curried: ', result2)
